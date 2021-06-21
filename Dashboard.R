f9f2fb7a3d2fc1b36a4107d4263f976756864082
Baudouin de Préval <deprevalbaudouin@gmail.com>
2021-06-21 14:28:07 +0200

    RNA-seq V1

--- Dashboard.R
+++ Dashboard.R
@@ -1,84 +1,24 @@
+## RNA_seq Raw Data
 library(shiny)
 library(shinydashboard)
-library(knitr)
-### Erreur de parenthèses
+library(ggplot2)
 
-ui <- dashboardPage(
-    skin = "green",
-    dashboardHeader(title = "Dermatose nodulaire bovine", titleWidth = 300),
-    dashboardSidebar(
-      sidebarMenu(title = "Menu",
-        menuItem("Importation des données", tabName = "données", icon = icon("import")),
-        menuItem("Vue d'ensemble", tabName = "ensemble", icon = icon("th")),
-        menuItem("Analyse", tabName = "analyse", icon = icon("stats")),
-        menuItem("Rapport", tabName = "rapport", badgeLabel = "Nouveau", badgeColor = "red", icon = icon("paperclip")),
-        menuItem("Code source", href = "https://github.com/Baud-de-Preval/", icon = icon("file-code-o"))
-    )
+ui <- dashboardPage(skin = "red", dashboardHeader(title = "Visualisation des données brutes", titleWidth = 400),
+  dashboardSidebar(
+    menuItem("Importation des données", tabName = "data", icon = icon("glyphicon glyphicon-floppy-open")),
+    menuItem("Données brutes", tabName = "raw", icon = icon("glyphicon glyphicon-stats"))
+  ))
+  dashboardBody(
+    fluidRow(
+      box(status = "success",width = 6, solidHeader = TRUE, fileInput(inputId = "RNA-seq", label = "Charger vos données ici", accept = ".txt")),
+      box(plotOutput(inputId = "Plot1", height = 300))
   )
 )
-###  
-  dashboardBody(
-    tabItems(
-  skin = "green",
-  dashboardHeader(title = "Dermatose nodulaire bovine"),
-  dashboardSidebar(
-    sidebarMenu(title = "Menu",
-      menuItem("Entrée des données", tabName = "données", icon = icon("dashboard")),
-      menuItem("Vue d'ensemble", tabName = "ensemble", icon = icon("dashboard")),
-      menuItem("Analyse", tabName = "analyse", icon = icon("dashboard")),
-      menuItem("Rapport", tabName = "rapport", icon = icon("dashboard"))
-      ))
-    ),
-   dashboardBody(
-    fluidRow(tabItems(
-      tabItem(tabName = "données",
-        fluidRow(column(3 ,align ="center"),
-          box(status = "success",width = 6, solidHeader = TRUE, fileInput(inputId = "data", label = "Charger vos données ici")))),
-      
-      tabItem(tabName = "Vue d'ensemble",
-        fluidRow(align ="center"),
-          tabBox(title = "Exploration des données", id = "explore",
-            tabPanel("Graphic representation"),
-            tabPanel("Summary"))),
-            tableOutput(outputId = "Graphe"),
-          
-      tabItem(tabName = "analyse",
-        fluidRow(align = "center"),
-        box(status = "info",width = 6, solidHeader = TRUE, tableOutput(outputId = "summary"), plotOutput(outputId = "plot", inline = TRUE))),
-              
-      tabItem(tabName = "rapport",
-          textOutput(outputId = "report"),
-          radioButtons('format', 'Format du document', c('PDF', 'HTML', 'Word'), inline = TRUE),
-          downloadButton(outputId = "download", label = "Télécharger")),
-     
-       shinyUI(
-        fluidPage(
-          uiOutput('markdown')
-        )
-    )),
-          radioButtons('format', 'Format du document', c('PDF', 'HTML', 'Word'), inline = TRUE),
-          downloadButton(outputId = "download", label = "Télécharger"))))
-
 
 server <- function(input, output) {
-  output$summary <- renderTable({
-    summary(input$data)}) 
-  output$plot <- renderPlot({
-    hist(input$data)})
-  #data <- #Le rapport
-  output$report <- renderUI({
-    HTML(markdown::markdownToHTML(knit('RMarkdownFile.rmd')))
-    })
-
-  output$download <- downloadHandler(
-    filename = function() {
-      paste("Rapport", Sys.Date(), ".txt", sep="")
-    },
-    content = function(file) {
-      write(data, file)
-    }
-  )
+  output$Plot1 <- renderPlot({
+    file <-input$RNA-seq
+  })
 }
-shinyApp(ui = ui, server = server)
 
-
+shinyApp(ui, server)
