282f5f1521f6a16710f8fc3b2e44969e3aa95864
Baudouin de Préval <deprevalbaudouin@gmail.com>
2021-06-21 08:49:10 +0200

    Deuxième version

--- Dashboard.R
+++ Dashboard.R
@@ -1,32 +1,60 @@
 library(shiny)
 library(shinydashboard)
+library(knitr)
+### Erreur de parenthèses
 
-ui <- dashboardPage(skin = "green",
-  dashboardHeader(title = "Dermatose nodulaire bovine"),
-  dashboardSidebar(sidebarMenu(title = "Menu"),
-               menuItem("Entrée des données", tabName = "données", icon = icon("dashboard")),
-               menuItem("Vue d'ensemble", tabName = "ensemble", icon = icon("dashboard")),
-               menuItem("Analyse", tabName = "analyse", icon = icon("dashboard")),
-               menuItem("Rapport", tabName = "rapport", icon = icon("dashboard"))))
-
-  dashboardBody(
-    fluidRow(tabItems(
+ui <- dashboardPage(
+    skin = "green",
+    dashboardHeader(title = "Dermatose nodulaire bovine", titleWidth = 300),
+    dashboardSidebar(
+      sidebarMenu(title = "Menu",
+        menuItem("Importation des données", tabName = "données", icon = icon("import")),
+        menuItem("Vue d'ensemble", tabName = "ensemble", icon = icon("th")),
+        menuItem("Analyse", tabName = "analyse", icon = icon("stats")),
+        menuItem("Rapport", tabName = "rapport", badgeLabel = "Nouveau", badgeColor = "red", icon = icon("paperclip")),
+        menuItem("Code source", href = "https://github.com/Baud-de-Preval/", icon = icon("file-code-o"))
+    )
+  )
+)
+###  
+   dashboardBody(
+    tabItems(
       tabItem(tabName = "données",
-              box(fileInput(inputId = "data", label = "Charger vos données ici"))),
+        fluidRow(column(3 ,align ="center"),
+          box(status = "success",width = 6, solidHeader = TRUE, fileInput(inputId = "data", label = "Charger vos données ici")))),
+      
       tabItem(tabName = "Vue d'ensemble",
-              textOutput(outputId = "summary"), plotOutput(outputId = "plot"))),
+        fluidRow(align ="center"),
+          tabBox(title = "Exploration des données", id = "explore",
+            tabPanel("Graphic representation"),
+            tabPanel("Summary"))),
+            tableOutput(outputId = "Graphe"),
+          
       tabItem(tabName = "analyse",
-              tableOutput(outputId = "Graphe")),
+        fluidRow(align = "center"),
+        box(status = "info",width = 6, solidHeader = TRUE, tableOutput(outputId = "summary"), plotOutput(outputId = "plot", inline = TRUE))),
+              
       tabItem(tabName = "rapport",
-              radioButtons('format', 'Format du document', c('PDF', 'HTML', 'Word'), inline = TRUE),
-              downloadButton(outputId = "download", label = "Télécharger"))))
+          textOutput(outputId = "report"),
+          radioButtons('format', 'Format du document', c('PDF', 'HTML', 'Word'), inline = TRUE),
+          downloadButton(outputId = "download", label = "Télécharger")),
+     
+       shinyUI(
+        fluidPage(
+          uiOutput('markdown')
+        )
+    )))
 
 server <- function(input, output) {
   output$summary <- renderTable({
-    summary(input$data)})
+    summary(input$data)}) 
   output$plot <- renderPlot({
-    hist(imput$data)})
+    hist(input$data)})
   #data <- #Le rapport
+  output$report <- renderUI({
+    HTML(markdown::markdownToHTML(knit('RMarkdownFile.rmd')))
+    })
+
   output$download <- downloadHandler(
     filename = function() {
       paste("Rapport", Sys.Date(), ".txt", sep="")
@@ -37,3 +65,5 @@ server <- function(input, output) {
   )
 }
 shinyApp(ui = ui, server = server)
+
+
