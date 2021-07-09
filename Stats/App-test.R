8124c1389ddcc7ac42c149485b189527c3d4a2e9
Baudouin de Préval <86061613+Baud-de-Preval@users.noreply.github.com>
2021-07-09 17:41:44 +0200

    Update App-test.R

--- Stats/App-test.R
+++ Stats/App-test.R
@@ -2,6 +2,9 @@
 
 library(readxl)
 library(tidyverse)
+library(reshape2)
+library(plyr)
+
 pep1 <- read_excel("Téléchargements/Li2D_MS21-001_Table S1_List of PSMs.xlsx")
 pep <- pep1[,-12]
 
@@ -21,16 +24,10 @@ distinct(dplyr::filter(pep_uni, grepl('Bos taurus]', Species)), Species) # 1669 
 # 1.2 Tableau des peptides
 
 df <- read_excel("work/R_script/Peptide representation.xlsx")
-df <- df[,-24]
+df %>% unite("description", 22:24,remove = T) -> df
 df <- separate(df,Origine_Query, "Query", sep = 6)
 df_order <- df %>% group_by(Accession) %>% top_n(1, 'Mascot Peptide Score')
 df_uni <- distinct(summarise(df_order)) # --> Nb correspondant au tableau des protéines
-
-df_LSD <- distinct(dplyr::filter(df_order, grepl('Lumpy skin disease virus', Species))) 
-df_Bos <- distinct(dplyr::filter(df_order, grepl('Bos taurus]', Species)))
-summarise(df_LSD)
-summarise(df_Bos) # On retrouve les données de 1.1
-
 
 df_untidy <- df %>% pivot_wider(names_from = Query, values_from = `Mascot Peptide Score`)
 df_untidy <- mutate_at(df_untidy, colnames(df_untidy[,22:30]), ~replace(., is.na(.), 0))
@@ -55,7 +52,11 @@ N <- c(x1,x2,x3,x4,x5,x6,x7,x8,x9) # Nombre de protéine détectée dans tous le
 Sample <- row.names(df_sum)
 V <- data.frame(N, colSums(df_untidy[,22:30]))
 
-ggplot(data = V,  mapping = aes(y= N, x= Sample, fill = colSums.df_untidy...22.30..)) + # Le graphe n'affiche aucune donnée
+V %>% ggplot(mapping = aes(y= N, x= Sample, color = V[,2])) +
+  geom_bar(fill = "steelblue", stat = "identity") +
+  labs(color="Mascot score") +
+  scale_color_gradient(low="blue", high="red") +
+  geom_text(aes(label=N), position=position_dodge(width=0.9), vjust=-0.25) +
   theme(axis.text.x = element_text(face="bold", color="black",angle = 32)) +
   ggtitle("Number of protein in each sample") + 
   xlab("Sample") + 
@@ -64,58 +65,52 @@ ggplot(data = V,  mapping = aes(y= N, x= Sample, fill = colSums.df_untidy...22.3
 ## Protéines trouvées dans chaque échantillon
 # Pour chaque échantillon, nb de protéine et de peptide qui les forment
 
-Nb_pep<-matrix(rep(0,36), ncol=9, nrow=4, byrow=FALSE) # Création d'une matrice vierge
+Nb_pep<-matrix(rep(0,36), ncol=9, nrow=5, byrow=FALSE) # Création d'une matrice vierge
 colnames(Nb_pep) = Sample
-rownames(Nb_pep) = c("1 peptide", "2 peptides", "3 peptides", "plus de 3 peptides")
+name = c("1 peptide", "2 to 5 peptides", "5 to 20 peptides", " 20 to 50 peptides","more than 50 peptides")
+rownames(Nb_pep)<- name
+df3 <- df %>% group_by(Accession, Query, description) %>% 
+  select(sequence) %>% unique 
 
-df %>% group_by(sequence) %>% top_n(1, `Mascot Peptide Score`) # Gérer les doublons avec max(MPS)
-
-for (k in 1:9){
-  df_3 <- filter(df, Query == Sample[k])
-  for (l in table(factor(df_3$sequence))){
-    return(l)
-    if (l <= 3){
-    Nb_pep[l,k] = Nb_pep[l,k] +1
-    }
-    else {
-      Nb_pep[4,k] =Nb_pep[l,k] +1
-    } 
-  }
+i=0
+for (l in 1:9){
+  i=i+1
+  df4 <- subset(df3,Query == Sample[l])
+  nb = as.numeric(table(df4$Accession))
+  Nb_pep[1,i] <- sum(nb==1)
+  Nb_pep[2,i] <- sum(nb>1 & nb<= 5)
+  Nb_pep[3,i] <- sum(nb>5 & nb<=20)
+  Nb_pep[4,i] <- sum(nb>20 & nb<=50)
+  Nb_pep[5,i] <- sum(nb>50)
 }
+Nb_pep
+NB <-data.frame(Nb_pep)
+NB <- pivot_longer(NB, Sample, names_to = "Trial", values_to = "Number")
 
- # Pour l'instant renvoie une matrice Nb_pep incorrecte
-
-ggplot(data = V,  mapping = aes(y= N, x= Sample, fill = colSums.df_untidy...22.30..)) + 
-  theme(axis.text.x = element_text(face="bold", color="black",angle = 32)) +
-  ggtitle("Number of protein in each sample") + 
-  xlab("Sample") + 
-  ylab("Cumulated number of protein") +
-  facet_wrap(~Sample)
+p = ggplot(data = data.frame(NB), mapping = aes(y= unlist(NB[,2]), x= rep(rownames(Nb_pep),9), levels = rep(name, 9)), fill =Trial) +
+  geom_bar(stat = "identity") +
+  theme(axis.text.x = element_text(face="bold", color="black", size = 6)) +
+  geom_text(aes(label=Number), position=position_dodge(width=0.9), vjust=-0.25, size = 2.5) + 
+  ggtitle("Number of peptide composing proteins") + 
+  ylab("Protein composition")+ 
+  theme(legend.position="none") +
+  xlab("")+
+  facet_wrap(~ Trial, scales = "free_x") +
+  aes(fill = as.factor(Trial)) 
+p
 
 ###
+# Nombre de protéines par échantillons et par type
 
-# Trouver un moyen de compter le nb de protéine unique pour chaque échantillon
+df5 <- df3 %>% mutate("Organism")
+
+LSD <- sapply(df5, grep, pattern = "Lumpy")
+Bos2 <- matrix(sapply(df5, grep, pattern = "ovin"))
+Bos1 <- matrix(sapply(df5, grep, pattern = "taurus"))
+Bos <- join(Bos1, Bos2)
+# Comparer les échantillons par ACM, ACP ?
+
 
 nrow(distinct(dplyr::filter(df_untidy2, grepl('Lumpy skin disease virus', Accession))) )
 nrow(is.na(df_untidy2$Q27824))
-
-ggplot(data = data.frame(Sc_tot), mapping =  aes(fill = NSAF_tot, y=Sc_tot, x= rawprot$Protein)) + 
-  theme(axis.text.x = element_text(face="bold", color="black",size=7, angle=45)) +
-  geom_bar(position="stack", stat="identity") +
-  labs(fill="NSAF") +
-  scale_fill_gradient(low="blue", high="red") +
-  ggtitle("Raw data from the 9 experiments") + 
-  xlab("Protein accession") + 
-  ylab("Cumulated spectral count") 
-
-# Garder les scores supérieurs à un seuil fixé
-
-pep_clear <- pep_uni$`Mascot Peptide Score` > 40
-summary(pep_clear)
-pep_clean <- pep_uni[pep_clear,]
-
-pep_virus <- pep_clean %>% filter(Species(starts_with("Lumpy")))
-
-
-
 
