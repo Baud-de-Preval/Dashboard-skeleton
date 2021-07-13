c459d1d25e7de9837274c9adbf4a1bb636c68aa6
Baudouin de Préval <86061613+Baud-de-Preval@users.noreply.github.com>
2021-07-13 16:14:26 +0200

    Update Dashboard.R

--- Dashboard.R
+++ Dashboard.R
@@ -1,99 +1,106 @@
-## RNA_seq Raw Data
 library(shiny)
 library(shinydashboard)
 library(knitr)
 library(tidyverse)
+library(readxl)
+library(reshape2)
+library(plyr)
+library(janitor)
 
-.theme<- theme(
-  axis.line = element_line(colour = 'gray', size = .75),
-  panel.background = element_blank(),
-  plot.background = element_blank()
-)
+# Initialisation
+
+
 
 ui <- dashboardPage(
   skin = "green", 
-  dashboardHeader(title = "Visualisation des données brutes", titleWidth = 300),
+  dashboardHeader(title = "Lumpy skin disease data analysis", titleWidth = 300),
   dashboardSidebar(sidebarMenu(
-    menuItem("Importation des données", tabName = "data", icon = icon("file")),
-    menuItem("Données brutes", tabName = "raw", icon = icon("area-chart")),
-    menuItem("Données groupées", tabName = "gathered", icon = icon("bar-chart")),
-    menuItem("Code source", tabName = "source", icon = icon("code"))
+    menuItem("Data uploader", tabName = "data", icon = icon("file")),
+    menuItem("Raw data", tabName = "raw", icon = icon("area-chart")),
+    menuItem("Grouped data", tabName = "gathered", icon = icon("bar-chart"), sliderInput("MPS", "MPS lower threshold :", 0,150,60),
+             menuSubItem("By sample", tabName = "sample", icon = icon("list-alt"))
+),
+    menuItem("Source code", tabName = "source", icon = icon("code"))
     # tags$img(class = "Image", src ="https://p4.wallpaperbetter.com/wallpaper/670/178/355/dna-spiral-genetics-twisted-wallpaper-preview.jpg")
   )),
-
+  
   dashboardBody(
     tabItems(
       tabItem(tabName = "data",
-      fluidRow(
-        box(status = "success",width = 6, solidHeader = TRUE, fileInput(inputId = "file", label = "Charger vos données ici",multiple = TRUE, accept = c(".txt","text/csv",
-                "text/comma-separated-values,text/plain",".csv",".ods"))),
-        box(tableOutput("contents"),width = 6, title = "Aperçu des données", status = "success", soliderHeader = TRUE, collapsible = T, collapsed = T),
-        box(checkboxInput("header", "Header", TRUE)),
+              fluidRow(
+                box(status = "success",width = 6, solidHeader = TRUE, fileInput(inputId = "file", label = "Please upload your data in xlsx or csv format", multiple = TRUE, accept = c("csv","xlsx"))),
+                box(tableOutput("contents"),width = 6, title = "Data overview", status = "success", soliderHeader = TRUE, collapsible = T, collapsed = T),
+                box(checkboxInput("header", "Header", TRUE)),
+                
+                # Input: Select separator ----
+                box(radioButtons("sep", "Separator",
+                                 choices = c(Comma = ",",
+                                             Semicolon = ";",
+                                             Tab = "\t"),
+                                 selected = ","),
+                    
+                    # Input: Select quotes ----
+                    radioButtons("quote", "Quote",
+                                 choices = c(None = "",
+                                             "Double Quote" = '"',
+                                             "Single Quote" = "'"),
+                                 selected = '"'),
+                    
+                    radioButtons("disp", "Display",
+                                 choices = c(Head = "head", All = "all"),
+                                 selected = "head")
+                    
+                )
+                
+                )
+              ),
       
-          # Input: Select separator ----
-        box(radioButtons("sep", "Separator",
-                     choices = c(Comma = ",",
-                                 Semicolon = ";",
-                                 Tab = "\t"),
-                     selected = ","),
-        
-        # Input: Select quotes ----
-        radioButtons("quote", "Quote",
-                     choices = c(None = "",
-                                 "Double Quote" = '"',
-                                 "Single Quote" = "'"),
-                     selected = '"'),
-   
-        radioButtons("disp", "Display",
-                     choices = c(Head = "head", All = "all"),
-                     selected = "head")
-        
+      tabItem("raw",
+        fluidRow(
+          box(tableOutput("Summary"), title = "Summary", status = "success", solidHeader = TRUE)
+        )
+          
         ),
       
-          
-    )),
-       
-     tabItem(tabName = "raw",
-      fluidRow(
-        box(width = '100%', plotOutput("graphe"), title = "Visualisation des données", status = "success", tableOutput("rawData"))
-          )
-      ),
-    
-    tabItem(tabName = "gathered",
-              box(width = '100%', plotOutput("ggraphe"), title = "Données groupées par variable", status = "success")
-            
-      ),
-    
-    tabItem(tabName = "source",
-            fluidRow(
-              box( status = "success", title = "Récupérer les résultats", width =6, solidHeader = TRUE, downloadButton("rapport","Rapport"), align="center"),
-              box( status = "success", title = "Récupérer le code", width = 6, solidHeader = TRUE, uiOutput("code"), align="center")
+      tabItem("sample",
+              fluidRow(
+                box(plotoutput = "datahard", status = "success", title = "Overview on raw data", solidHeader = TRUE),
+                box(title = "Samples display",
+                     (checkboxInput("Q27824","Q27824",FALSE)),
+                     (checkboxInput("Q27825","Q27825",FALSE)),
+                     (checkboxInput("Q27826","Q27826",FALSE)),
+                     (checkboxInput("Q27827","Q27827",FALSE)),
+                     (checkboxInput("Q27828","Q27828",FALSE)),
+                     (checkboxInput("Q27829","Q27829",FALSE)),
+                     (checkboxInput("Q27830","Q27830",FALSE)),
+                     (checkboxInput("Q27831","Q27831",FALSE)),
+                     (checkboxInput("Q27832","Q27832",FALSE)),
+                     (checkboxInput("all","All",TRUE))
+              )
+            )),
+      
+      tabItem(tabName = "source",
+              fluidRow(
+                box( status = "success", title = "Get the report", width =6, solidHeader = TRUE, downloadButton("rapport","Rapport"), align="center"),
+                box( status = "success", title = "Get the code", width = 6, solidHeader = TRUE, uiOutput("code"), align="center")
+              )
             )
-      )
-    ))
-)
-  
+          
+      
+        
+      )))
     
+  
 
 server <- shinyServer(function(input, output,session) {
   tab <- reactive({ 
     req(input$file)
-
-     tryCatch(
-      {
-        df <- read.csv(input$file$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote) 
-        df %>%
-          pivot_longer(c(`Dengue24ha`,`Dengue24hb`,`Dengue24hc`,`Dengue6da`,`Dengue6db`,`Dengue6dc`,`MOCKA24ha`,`MOCKA24hb`,`MOCKA24hc`,`MOCKA6da`,`MOCKA6db`,`MOCKA6dc`,`MOCKB24ha`,`MOCKB24hb`,`MOCKB24hc`,`MOCKB6da`,`MOCKB6db`,`MOCKB6dc`,`MOCKC24ha`,`MOCKC24hb`,`MOCKC24hc`,`MOCKC6da`,`MOCKC6db`,`MOCKC6dc`,`RVF24ha`,`RVF24hb`,`RVF24hc`,`RVF6da`,`RVF6db`,`RVF6dc`), names_to = "Samplename", values_to = "count")
-
-      },
-      error = function(e) {
-        # return a safe Error if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
+    
+    
+    df <- read.csv(input$file$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
     
     if(input$disp == "head") {
       return(head(df))
@@ -101,69 +108,75 @@ server <- shinyServer(function(input, output,session) {
     else {
       return(df)
     }
-
-  })
-  
-  output$contents <- renderTable({
-    rep(head(tab()))
+    
+    output$contents <- renderTable({
+      rep(head(df))
     })
-
-  output$graphe <- renderPlot({
-    Tot <- data.frame(colSums(tab()[-1]))
-    tab() %>%
-      ggplot(data =Tot, mapping = aes(x = colnames(tab()[-1]), y = colSums(tab()[-1])), fill = ) +
-      geom_bar(position="dodge", stat="identity", fill="steelblue") +
-      ggtitle("Nombre de gène portés par chaque échantillon") + 
-      xlab("Echantillon") + 
-      ylab("Gènes portés") +
-      theme(axis.text.x = element_text(face="bold", color="#543333",size=9, angle=90))
+    
+    # Traitement du tableau d'entrée
+    df %>% separate(Origine_Query,into=c("sample","query"),sep=6) -> df
+    df %>% group_by(sample,Accession,sequence) %>% unique -> df
+    Sample <- unique(df$sample)
+    N = as.numeric(table(df$sample))
+    
+    
+ 
+    output$Summary <- renderPlot({
+      df %>% ggplot(mapping = aes(y= N, x= Sample,color = colSums(tab()$'Mascot Peptide Score'))) +
+        geom_bar(fill = "steelblue", stat = "identity") +
+        labs(color="Mascot score") +
+        scale_color_gradient(low="blue", high="red") +
+        geom_text(aes(label=N), position=position_dodge(width=0.9), vjust=-0.25) +
+        theme(axis.text.x = element_text(face="bold", color="black",angle = 32)) +
+        ggtitle("Number of protein in each sample") + 
+        xlab("Sample") + 
+        ylab("Number of protein") 
     })
-  
-  ## Forme de la donnée ? df %>% pivot_longer
-  sv <- reactive({ 
-    req(input$file)
     
-      df2 <- read.csv(input$file$datapath,
-                      header = input$header,
-                      sep = input$sep,
-                      quote = input$quote)
-      l <- df2 %>% 
-      pivot_longer(c(`Dengue24ha`,`Dengue24hb`,`Dengue24hc`,`Dengue6da`,`Dengue6db`,`Dengue6dc`,`MOCKA24ha`,`MOCKA24hb`,`MOCKA24hc`,`MOCKA6da`,`MOCKA6db`,`MOCKA6dc`,`MOCKB24ha`,`MOCKB24hb`,`MOCKB24hc`,`MOCKB6da`,`MOCKB6db`,`MOCKB6dc`,`MOCKC24ha`,`MOCKC24hb`,`MOCKC24hc`,`MOCKC6da`,`MOCKC6db`,`MOCKC6dc`,`RVF24ha`,`RVF24hb`,`RVF24hc`,`RVF6da`,`RVF6db`,`RVF6dc`), names_to = "Samplename", values_to = "count")
-      m <- l %>% 
-          separate(Samplename, into = c("Samplename","Rep"), sep =-1)
-      n <- m %>% 
-          mutate(Samplename = stringr::str_replace(Samplename, "6d", "6da"))
-      o <- n %>%
-          separate(Samplename, into = c("Samplename","Time"), sep = -3)
-      return(o)
- })
-  
-    output$ggraphe <- renderPlot({
-      ggplot(sv(), aes(color=Samplename, group = Samplename, y=Gene, x=count)) + 
-      geom_density(adjust=0.1, aes(x = count, y = Samplename), stat = "identity") +
-      geom_bar(position="dodge", stat="identity") 
-})
-    
-  url <- a("ici", href="https://github.com/Baud-de-Preval/Dashboard-skeleton/blob/master/Dashboard.R")
-  output$code <- renderUI({
-    tagList("Le lien vers le code :", url)
-  })
-  
-  output$rapport <- downloadHandler(
-    filename = "Rapport.html",
-    content = function(file) {
-      tempReport <- file.path(tempdir(), "Rapport.rmd")
-      file.copy("Rapport.rmd", tempReport, overwrite = TRUE)
-      # A compléter
-      params <- list(n = input$slider)
-    
-      rmarkdown::render(tempReport, output_file = file,
-                        params = params,
-                        envir = new.env(parent = globalenv())
-      )
-    }
-  )
-  
+    output$sample <- renderplot({    ## Est ce que le nb d'échantillon sera tjrs constant ?
+      Essay <- c("Q27824", "Q27825", "Q27826", "Q27827", "Q27828", "Q27829", "Q27830", "Q27831", "Q27832")
+      if ("all" == T){
+        Nb = 9
+      }
+      else { 
+        Nb =table(Essay[TRUE])    # Déterminer le nombre d'échantillon selectionné
+      }
+      Nb_pep<-matrix(rep(0,ncol*nrow), ncol=Nb, nrow=5, byrow=FALSE) # Création d'une matrice vide
+      colnames(Nb_pep) = Sample
+      name = c("1 peptide", "2 to 5 peptides", "5 to 20 peptides", " 20 to 50 peptides","more than 50 peptides")
+      rownames(Nb_pep)<- name
+      
+      Essay <-Essay[TRUE]
+      i=0                         # Remplir une matrice nb peptide/prot/échantillon
+      for (l in 1:Nb){
+        i=i+1
+        df <- subset(df, Query == Essay[TRUE][l])
+        nb = as.numeric(table(df$Accession))
+        Nb_pep[1,i] <- sum(nb==1)
+        Nb_pep[2,i] <- sum(nb>1 & nb<= 5)
+        Nb_pep[3,i] <- sum(nb>5 & nb<=20)
+        Nb_pep[4,i] <- sum(nb>20 & nb<=50)
+        Nb_pep[5,i] <- sum(nb>50)
+      }
+      Nb_pep %>% pivot_longer(sample, names_to = "Trial", values_to = "Number") -> Nb_pep
+      
+      df %>% ggplot(mapping = aes(y= unlist(Nb_pep[,2]), x= rep(rownames(Nb_pep),Nb), levels = rep(name, Nb)), fill =Essay[TRUE]) +
+        geom_bar(stat = "identity") +
+        theme(axis.text.x = element_text(face="bold", color="black", size = 6)) +
+        geom_text(aes(label=Number), position=position_dodge(width=0.9), vjust=-0.25, size = 2.5) + 
+        ggtitle("Number of peptide composing proteins") + 
+        ylab("Protein composition")+ 
+        theme(legend.position="none") +
+        xlab("")+
+        facet_wrap(~ Trial, scales = "free_x") +
+        aes(fill = as.factor(Trial)) 
+     
+    })
+})     
+    url <- a("ici", href="https://github.com/Baud-de-Preval/Dashboard-skeleton/blob/master/Dashboard.R")
+    output$source <- renderUI({
+      tagList("Le lien vers le code :", url)
+    })
 })
 
 shinyApp(ui, server)
