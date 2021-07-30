488ad55d3c8e4a8dc4b3cba561f74f635c3197c0
Baudouin de Préval <86061613+Baud-de-Preval@users.noreply.github.com>
2021-07-30 16:52:37 +0200

    Update nm-MDS.R

--- Stats/nm-MDS.R
+++ Stats/nm-MDS.R
@@ -1,4 +1,4 @@
-### Non-metric MDS
+### Non-metric MDS & Venn Diagram
 
 library(magrittr)
 library(dplyr)
@@ -8,7 +8,6 @@ library(readxl)
 library(MASS)
 library(janitor)
 library(VennDiagram)
-library(vegan)
 
 samples = readxl::read_xlsx("Téléchargements/Li2D_MS21-001_Table S2_List of Proteins(1).xlsx",
                             skip=17,n_max=1,col_names = F)
@@ -40,35 +39,68 @@ for(i in 1:length(samples)){
 # Réduction des données aux prot > 75%
 
 finaldata %>% filter(finaldata$SC != 0) -> finaldata
-as.data.frame(table(finaldata$`Protein accession`)) %>% dplyr::filter(Freq >= round(0.75*length(samples))) -> finaldata75
+as.data.frame(table(finaldata$`Protein accession`)) %>% dplyr::filter(Freq >= round(0.90*length(samples))) -> finaldata75
 finaldata %>% filter(`Protein accession` %in% finaldata75$Var1) -> finaldata75
 
+# Diagramme de Venn
 
-mds <- tibble(finaldata75[,17:19])
-rownames(mds) <- make.names(finaldata75[,16], unique = TRUE)
-Prot.mds <- metaMDS(comm = mds, distance = "bray", trace = FALSE, autotransform = FALSE)
+Species <-as.list(finaldata %>% count(Organism) %>% unique)
 
-
-
-### Diagramme de Venn
-
-# Récupération des espèces
-Species <-as.vector(finaldata %>% count(Organism) %>% unique)[,1]
 finaldata %>% filter(Organism == Species[1]) -> x_data
 finaldata %>% filter(Organism == Species[2]) -> y_data
 
-# Protéines en communs
+if (length(x_data) > length(y_data)){
+  select(y_data$`Protein accession` %in% x_data$`Protein accession` ) -> Com_data
+  
+} else {
+  select(x_data$`Protein accession` %in% y_data$`Protein accession` ) -> Com_data
+}
+
+
 finaldata$`Protein accession` %>% intersect(x = x_data, y = y_data) %>% filter() -> Com_data
+
 
 nb = dplyr::filter(as.data.frame(x_data$`Protein accession`)) %>% unique %>% count
 nl = as.data.frame(y_data$`Protein accession`) %>% filter %>% unique %>% count
 n = as.data.frame(Com_data$`Protein accession`) %>% filter %>% unique %>% count
 
-# Affichage
 grid.newpage()
-draw.pairwise.venn(nb, nl, n, category = c(Species[1], Species[2]), lty = rep("blank", 2),
-                   fill = c("light blue", "pink"), alpha = rep(0.5, 2), cat.pos = c(0, 0), cat.dist = rep(0.05, 2), scaled = FALSE)
+draw.pairwise.venn(nb, nl, n, category = c(Species), lty = rep("blank", 2),
+                   fill = c("light blue", "pink"), alpha = rep(0.5, 2), cat.pos = c(0, 0), cat.dist = rep(0.025, 2), scaled = FALSE)
 
+# Tentative MDS - Brad Curtis
 
+mds <- as.matrix(finaldata75[,16:19])   # Puissance insuffisante
+d <- dist(mds)
+fit <- isoMDS(d, k = 2)
 
+colnames(mds) <- c("Dim.1", "Dim.2")
 
+x <- fit$points[,1]
+y <- fit$points[,2]
+plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2",
+     main="Nonmetric MDS", type="n")
+text(x, y, labels = row.names(mydata), cex=.7) 
+
+# Tentative MDS - Jaccard
+
+mds <- as.matrix(finaldata[,16:19])
+
+      #weighted jaccard similarity matrix setup
+sim.jac <- matrix(0, nrow=nrow(mds), ncol=nrow(mds))
+rownames(sim.jac) <- rownames(mds)
+colnames(sim.jac) <- rownames(mds)
+
+      #weighted jaccard function
+pairs <- t(combn(1:nrow(mds), 2))
+for (i in 1:nrow(pairs)){
+  num <- sum(sapply(1:ncol(mds), function(x)(min(mds[pairs[i,1],x],mds[pairs[i,2],x]))))
+  den <- sum(sapply(1:ncol(mds), function(x)(max(mds[pairs[i,1],x],mds[pairs[i,2],x]))))
+  sim.jac[pairs[i,1],pairs[i,2]] <- num/den
+  sim.jac[pairs[i,2],pairs[i,1]] <- num/den  
+}
+sim.jac[which(is.na(sim.jac))] <- 0
+diag(sim.jac) <- 1
+
+      #weighted jaccard distance
+dist.jac <- 1-sim.jac
